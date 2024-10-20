bf1bb35d70e573402e928dd0da317bc50aa65277
LucasRdeFreitas <lucasrfreitas0@gmail.com>
2024-10-20 18:47:50 -0300

    Implementação da tag de informação do sol

---
+++ TesteAR/Assets/Scripts/ShowTag.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTag : MonoBehaviour
+{
+    private float distance;
+    private Camera cam;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cam = GameObject.Find("ARCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance=Vector3.Distance(cam.transform.position, transform.position);
+        if (distance < 50)
+        {
+            transform.LookAt(cam.transform);
+            transform.localScale = new Vector3(-6.0f, 6.0f, 6.0f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+}
